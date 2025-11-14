41da8749cb350ba19201c5967efe390bb330953f
manny50-cpu <manuelgames30@gmail.com>
2025-11-14 15:32:39 +0000

    i love lists

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -101,3 +101,27 @@ print(food)
 food.reverse()
 print(food)
 # Create a list of 3 lists (matrix), and access the middle element.
+#sets = {1,2,3}
+#sets are unordered collections of unique items 
+# sets do not support indexing or slicing
+# sets are mutable, meaning you can add or remove items
+# sets are created using curly braces {}
+my_set = {1,2,3,4,5}
+print(my_set)
+print(type(my_set))
+my_set.add(6)
+print(my_set)
+my_set.remove(3)
+print(my_set)
+print( 4 in my_set) #true
+print(3 in my_set) #false
+
+# tuples are ordered collections of items
+# tuples are immutable, meaning you cnanot modify them after creation
+# typels are created using parentheses()
+my_tuple = (1,2, 3, 4, 5)
+print(my_tuple)
+print(type(my_tuple))
+print(my_tuple[0])
+print(my_tuple[1:4])
+
