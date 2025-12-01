de8f1d60ee071eddc9d1bc3551d0c46d9d90341f
manny50-cpu <manuelgames30@gmail.com>
2025-12-01 15:01:52 +0000

    Temperature added

--- week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
+++ week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
@@ -4,9 +4,22 @@
 # Scenario:
 # Write a program that:
 
-# Asks the user for today’s temperature.
+# Asks the user for today’s temperature. 
+temperature = float(input("What is the weather like?"))
 
 # Prints whether it’s cold, warm, or hot using comparison operators.
+if temperature>= 80 and temperature <=100:
+    print("Its hot outside ")
+elif temperature>=50 and temperature<=79:
+     print("Its warm outside")
+elif temperature <=-10 or temperature>=110:
+     print("Extreme temperature warning")
+else:
+     print("its cold outside")
+
+     
+        
+    
 
 # If the temperature is out of range (below -10 or above 110), display “Extreme temperature warning!”
 
